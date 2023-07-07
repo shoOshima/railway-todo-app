85736b72fab7d62b913ff6f3ab4aabd71db42de3
shoOshima <abookmarkmaster@gmail.com>
2023-07-08 01:02:44 +0900

    s3

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { Home } from "../pages/Home";
-import { NotFound } from "../pages/NotFound";
-import { SignIn } from "../pages/SignIn";
-import { NewTask } from "../pages/NewTask";
-import { NewList } from "../pages/NewList";
-import { EditTask } from "../pages/EditTask";
-import { SignUp } from "../pages/SignUp";
-import { EditList } from "../pages/EditList";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { NotFound } from '../pages/NotFound';
+import { SignIn } from '../pages/SignIn';
+import { NewTask } from '../pages/NewTask';
+import { NewList } from '../pages/NewList';
+import { EditTask } from '../pages/EditTask';
+import { SignUp } from '../pages/SignUp';
+import { EditList } from '../pages/EditList';
 
 export const Router = () => {
-  const auth = useSelector((state) => state.auth.isSignIn)
+  const auth = useSelector((state) => state.auth.isSignIn);
 
   return (
     <BrowserRouter>
@@ -23,14 +23,18 @@ export const Router = () => {
             <Route exact path="/" element={<Home />} />
             <Route exact path="/task/new" element={<NewTask />} />
             <Route exact path="/list/new" element={<NewList />} />
-            <Route exact path="/lists/:listId/tasks/:taskId" element={<EditTask />} />
+            <Route
+              exact
+              path="/lists/:listId/tasks/:taskId"
+              element={<EditTask />}
+            />
             <Route exact path="/lists/:listId/edit" element={<EditList />} />
           </>
         ) : (
-          <Route exact path="/"  element={<SignIn />} />
+          <Route exact path="/" element={<SignIn />} />
         )}
         <Route element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
